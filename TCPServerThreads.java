1a6fd357b5e810ff3da90337fcece3785eb2372c
aggielam26 <68656449+aggielam26@users.noreply.github.com>
2020-11-04 23:55:07 -0500

    Update TCPServerThreads.java

--- TCPServerThreads.java
+++ TCPServerThreads.java
@@ -1,91 +1,121 @@
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 class TCPServerThreads implements Runnable {
-    private Socket socket;
-    Message response;
-    boolean synReceived = false;
-    boolean endConnection = false;
-    String client;
+	private Socket socket;
+	Message response;
+	boolean synReceived = false;
+	boolean endConnection = false;
+	String client;
 
-    TCPServerThreads( Socket socket) {
-        this.socket = socket;
-        System.out.println("Creating ");
-    }
+	TCPServerThreads( Socket socket) {
+		this.socket = socket;
+		System.out.println("Creating ");
+	}
 
-    public void run() {
-        System.out.println("Running ");
-        while (!endConnection) {
+	public void run() {
+		System.out.println("Running ");
+		while (!endConnection) {
+			try {
+				DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+				InputStream inToClient = socket.getInputStream();               
+				
+				Message message = receiveMessage(inToClient);
 
-            DataOutputStream outToClient = null;
-            byte[] buffer = null;
-            try {
-                outToClient = new DataOutputStream(socket.getOutputStream());
-                InputStream inToClient = null;                
-                inToClient = socket.getInputStream();               
-                byte[] bytes = new byte[1024];           
-                inToClient.read(bytes);   
+				if(!synReceived) {
+					if(message.isSyn()) {
+						synReceived = true;
+						
+						//send syn-ack message to establish connection
+						response = new Message(client, true, true, false);
+					}
+					else {
+						System.out.println("Error. Invalid connection attempt");
+						endConnection = true;
+					}
+				}
+				else {
+					if(message.isFin()) {
+						response = new Message(client, false, true, true);
+						endConnection = true;
+					}
+					else {
+						try {
+							System.out.println("Message request: " + message.data + "\n");
+							response = solve(message);
+						} catch (ScriptException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				sendMessage(outToClient, response);
+			} 
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Thread ended with client "+client);
+		}
+	}
 
-                // read client request
-                Message message = null;
-                message = new Message(client, new String(bytes, StandardCharsets.UTF_8));
+	public static void sendMessage(OutputStream outputStream, Message message)
+	{
+		try
+		{
+			ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
+			ObjectOutputStream out = new ObjectOutputStream(byteOutStream);
+			
+			out.writeObject(message);
+			out.flush();
+			byte[] byteMessage = byteOutStream.toByteArray();
+			outputStream.write(byteMessage, 0, byteMessage.length); 
+			outputStream.flush();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 
-                if(!synReceived)
-                {
-                    if(message.getSyn() == true)
-                    {
-                        //send syn-ack message to establish connection
-                        response = new Message(client, true, true, false);
-                        buffer = Double.toString(response.answer).getBytes();
-                    }
-                    else
-                    {
-                        System.out.println("Error. Invalid connection attempt");
-                        endConnection = true;
-                    }
-                }
-                else
-                {
-                    if(message.getFin() == true)
-                    {
-                        response = new Message(client, false, true, true);
-                        endConnection = true;
-                    }
-                    else
-                    {
-                        try{
-                            System.out.println("Message request: " + message.data + "\n");
-                            response = solve(message);
-                            buffer = Double.toString(response.answer).getBytes();
-                        }
-                        catch (Exception e)
-                        {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                outToClient.write(buffer, 0, buffer.length);
-            } 
-            catch (IOException e) 
-            {
-                e.printStackTrace();
-            }
-            System.out.println("Thread ended with client "+client);
-        }
-    }
-
-    public Message solve(Message request) throws ScriptException {
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("JavaScript");
-        double resultNum = Double.parseDouble(engine.eval(request.data.trim()).toString());
-        Message response = new Message(client, request.data, resultNum);
-        return response;
-    }
+	public static Message receiveMessage(InputStream inputStream)
+	{
+		try
+		{
+			ByteArrayInputStream byteInStream;
+			ObjectInputStream in;
+			
+			byte[] bytes = new byte[1024];
+			inputStream.read(bytes);
+			byteInStream = new ByteArrayInputStream(bytes);
+			in = new ObjectInputStream(byteInStream);
+			return (Message) in.readObject();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public Message solve(Message request) throws ScriptException {
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("JavaScript");
+		double resultNum = Double.parseDouble(engine.eval(request.data.trim()).toString());
+		System.out.println("Result: " + resultNum);
+		
+		Message response = new Message(client, request.data, resultNum);
+		return response;
+	}
 
 }
